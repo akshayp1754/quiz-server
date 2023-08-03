f8b7bf690a8f0172d46734af626f02a653529ca3
akshayp1754 <akshayp1754@gmail.com>
2023-08-03 13:18:23 +0530

    registration

--- routes/registration.js
+++ routes/registration.js
@@ -2,15 +2,24 @@ const express = require("express");
 const router = express.Router();
 const UserModel = require("../models/signup");
 
-// Define a route for handling POST requests to "/user-register"
-router.post("/user-register", async (req, res) => {
-  try {
-    const addNewUser = await UserModel.create(req.body);
-    return res.json({ userAdded: addNewUser, message: "User was added" });
-  } catch (error) {
-    // If there's an error, handle it appropriately
-    return res.status(500).json({ error: "Internal Server Error" });
-  }
+
+router.route("/user-register").post( (req, res) => {
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+ 
+    const newUserModel = new UserModel({
+        username,
+        email,
+        password,
+      });
+ 
+      newUserModel.save()
+ 
 });
+
+
+
+
 
 module.exports = router;
